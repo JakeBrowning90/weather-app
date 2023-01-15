34185a56d260ae0cd33d5157a584fed97046ae1f
jakebrowning90 <jacobbrowning90@gmail.com>
2023-01-15 13:18:24 -0500

    Create function module template

---
+++ src/functionTemplate.js
@@ -0,0 +1,14 @@
+
+// import { functionName } from "./functionName";
+
+// function functionTemplate() {}
+
+// export {  functionTemplate };
+
+// import { functionName } from "./functionName";
+
+function functionTemplate() {
+    console.log("Template is working");
+}
+
+export { functionTemplate };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './reset.css';
 import './style.css';
+import { functionTemplate } from './functionTemplate.js'
 
 console.log("Hello world!");
+functionTemplate();
