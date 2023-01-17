035d8570792ce57f2891a7cdf965e60b1603e2cf
jakebrowning90 <jacobbrowning90@gmail.com>
2023-01-16 20:51:00 -0500

    Send values from async function Object to DOM

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import './style.css';
 import { getWeather } from './getWeather.js'
 // import { functionTemplate } from './functionTemplate.js'
 
+const weatherOutput = document.getElementById('weatherOutput');
+
 let weatherData = getWeather();
-weatherData.then((currentWeather) => console.log(currentWeather.condition));
+weatherData.then((currentWeather) => weatherOutput.textContent = currentWeather.condition);
 weatherData.then((currentWeather) => console.log(currentWeather.cTemp));
 weatherData.then((currentWeather) => console.log(currentWeather.fTemp));
+
