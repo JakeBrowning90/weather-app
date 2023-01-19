44274ed7b930d10753412c7fdd5ba0298411b692
jakebrowning90 <jacobbrowning90@gmail.com>
2023-01-18 22:22:02 -0500

    Toggle temperature display between C and F

--- src/getWeather.js
+++ src/getWeather.js
@@ -1,14 +1,22 @@
-let location = "boston,us";
+
 
 async function getWeather() {
+    const city = document.getElementById('cityField')
+    let location = city.value;
     const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=' + location + '&APPID=e8257bcdcd628305b554f62d782b0777', {mode: 'cors'});
     const weatherData = await response.json();
-    // console.log(weatherData);
+    console.log(weatherData);
     const currentWeather = new Object();
     //Add condition, celsius, and fahrenheit to object
+    currentWeather.icon = weatherData.weather[0].icon;
+    currentWeather.location = weatherData.name;
+    currentWeather.country = weatherData.sys.country;
     currentWeather.condition = weatherData.weather[0].main;
+    currentWeather.humidity = weatherData.main.humidity;
     currentWeather.cTemp = (weatherData.main.temp - 273.15).toFixed(1);
     currentWeather.fTemp = (((weatherData.main.temp - 273.15) * 9) / 5 + 32).toFixed(1);
+    currentWeather.cTempFeel = (weatherData.main.feels_like - 273.15).toFixed(1);
+    currentWeather.fTempFeel = (((weatherData.main.feels_like - 273.15) * 9) / 5 + 32).toFixed(1);
 
     // console.log(currentWeather);
     return currentWeather;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,62 @@ import './style.css';
 import { getWeather } from './getWeather.js'
 // import { functionTemplate } from './functionTemplate.js'
 
-const weatherOutput = document.getElementById('weatherOutput');
+const toggleButton = document.getElementById('toggleButton');
+const timeStamp = document.getElementById('timeStamp');
+const weatherIcon = document.getElementById('weatherIcon');
+const locationDiv = document.getElementById('locationDiv');
+const timeDiv = document.getElementById('timeDiv');
+const conditionDiv = document.getElementById('conditionDiv');
+const humidityDiv = document.getElementById('humidityDiv');
+const cTempDiv = document.getElementById('cTempDiv');
+const fTempDiv = document.getElementById('fTempDiv');
+const cTempFeelDiv = document.getElementById('cTempFeelDiv');
+const fTempFeelDiv = document.getElementById('fTempFeelDiv');
 
-let weatherData = getWeather();
-weatherData.then((currentWeather) => weatherOutput.textContent = currentWeather.condition);
-weatherData.then((currentWeather) => console.log(currentWeather.cTemp));
-weatherData.then((currentWeather) => console.log(currentWeather.fTemp));
+const form = document.getElementById('form');
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let weatherData = getWeather();
+    weatherData.then((currentWeather) => weatherIcon.src = "http://openweathermap.org/img/wn/" + currentWeather.icon + "@2x.png");
+    weatherData.then((currentWeather) => locationDiv.textContent = currentWeather.location + ", " + currentWeather.country);
+    weatherData.then((currentWeather) => conditionDiv.textContent = currentWeather.condition);
+    weatherData.then((currentWeather) => humidityDiv.textContent = currentWeather.humidity + "%");
+    weatherData.then((currentWeather) => cTempDiv.textContent = currentWeather.cTemp + "C");
+    weatherData.then((currentWeather) => fTempDiv.textContent = currentWeather.fTemp + "F");
+    weatherData.then((currentWeather) => cTempFeelDiv.textContent = "Feels like " + currentWeather.cTempFeel + "C");
+    weatherData.then((currentWeather) => fTempFeelDiv.textContent = "Feels like " + currentWeather.fTempFeel + "F");
+    timeStamp.textContent = "Last updated: " + new Date();
+});
+
+toggleButton.addEventListener('click', swapTemp);
+
+function swapTemp() {
+    console.log('swap!');
+    if (fTempDiv.className == 'hidden') {
+        cTempDiv.classList.add('hidden');
+        cTempFeelDiv.classList.add('hidden');
+        fTempDiv.classList.remove('hidden');
+        fTempFeelDiv.classList.remove('hidden');
+    } else if (cTempDiv.className == 'hidden') {
+        fTempDiv.classList.add('hidden');
+        fTempFeelDiv.classList.add('hidden');
+        cTempDiv.classList.remove('hidden');
+        cTempFeelDiv.classList.remove('hidden');
+       
+    }
+}
+
+
+// let weatherData = getWeather();
+// weatherData.then((currentWeather) => weatherIcon.src = "http://openweathermap.org/img/wn/" + currentWeather.icon + "@2x.png");
+// weatherData.then((currentWeather) => locationDiv.textContent = currentWeather.location);
+// weatherData.then((currentWeather) => conditionDiv.textContent = currentWeather.condition);
+// weatherData.then((currentWeather) => tempDiv.textContent = currentWeather.cTemp + "C / " + currentWeather.fTemp + "F");
+// timeDiv.textContent = Date();
+// weatherData.then((currentWeather) => console.log(currentWeather.cTemp));
+// weatherData.then((currentWeather) => console.log(currentWeather.fTemp));
+
+
+
+
 
