08a440b1c4a0d3881c0fa7cc126610dd79b87503
jakebrowning90 <jacobbrowning90@gmail.com>
2023-01-21 15:53:41 -0500

    Background image test 2

--- src/updateBG.js
+++ src/updateBG.js
@@ -1,17 +1,11 @@
-import Day from './img/white-cloud-blue-sky.jpg'
-import Dusk from './img/white-cloud-blue-sky.jpg'
-import Night from './img/white-cloud-blue-sky.jpg'
-import Dawn from './img/white-cloud-blue-sky.jpg'
-
 function updateBG(currentTime) {
     let currentHour = currentTime.getHours();
     if (currentHour >= 9 && currentHour <= 14) {
         //Set day image
-        document.body.style.backgroundImage = Day;
+        document.body.style.backgroundImage = "url(../src/img/white-cloud-blue-sky.jpg)";
     } else if (currentHour >= 15 && currentHour <= 20) {
         //Set dusk image
-        console.log("dusk");
-        document.body.style.backgroundImage = Dusk;
+        document.body.style.backgroundImage = "url(../src/img/nature-colorful-landscape-dusk-cloud.jpg)";
     } else if (currentHour >= 21 || currentHour <= 2) {
         //Set night image
         document.body.style.backgroundImage = "url(../src/img/paul-volkmer-qVotvbsuM_c-unsplash.jpg)";
