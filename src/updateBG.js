b3b4c800bf17dd9d54daea96601ee99f198cfa4a
jakebrowning90 <jacobbrowning90@gmail.com>
2023-01-21 16:09:48 -0500

    Change background images to classes

--- src/updateBG.js
+++ src/updateBG.js
@@ -2,16 +2,16 @@ function updateBG(currentTime) {
     let currentHour = currentTime.getHours();
     if (currentHour >= 9 && currentHour <= 14) {
         //Set day image
-        document.body.style.backgroundImage = "url(../src/img/white-cloud-blue-sky.jpg)";
+        document.body.classList.add('dayBG');
     } else if (currentHour >= 15 && currentHour <= 20) {
         //Set dusk image
-        document.body.style.backgroundImage = "url(../src/img/nature-colorful-landscape-dusk-cloud.jpg)";
+        document.body.classList.add('duskBG');
     } else if (currentHour >= 21 || currentHour <= 2) {
         //Set night image
-        document.body.style.backgroundImage = "url(../src/img/paul-volkmer-qVotvbsuM_c-unsplash.jpg)";
+        document.body.classList.add('nightBG');
     } else if (currentHour >= 3 && currentHour <= 8) {
         //Set dawn image
-        document.body.style.backgroundImage = "url(../src/img/sky-background-sunrise.jpg)";
+        document.body.classList.add('dawnBG');
     } 
 }
 
